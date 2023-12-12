b26c1ff5062d4f7f8325693fbd98784b59323ded
chenfly256 <ljkjsun@gmail.com>
2023-12-12 10:40:57 +0800

    chapter2

---
+++ Chapter2/software/ChenFly/Core/Src/voltage.c
@@ -0,0 +1,19 @@
+#include "voltage.h"
+#include "adc.h"
+#include "usart.h"
+
+uint16_t adcData;
+float voltage;
+float voltage_ratio = 1.025;
+uint8_t voltage_10;
+
+void voltage_read(void)
+{
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, 100);
+	adcData = HAL_ADC_GetValue(&hadc1);
+	HAL_ADC_Stop(&hadc1);
+	voltage = adcData*(3.3/4096)*11*voltage_ratio;
+	voltage_10 = (uint8_t)(voltage*10);
+	HAL_UART_Transmit(&huart1, &voltage_10, 1, 0xffff);
+}
---
+++ Chapter2/software/ChenFly/MDK-ARM/ChenFly/startup_stm32f405xx.d
@@ -0,0 +1,1 @@
+chenfly\startup_stm32f405xx.o: startup_stm32f405xx.s
